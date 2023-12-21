64b78c2fe27a6a79597a01a416d3477963f81aea
Martin Alemajoh <alemajohmartin@gmail.com>
2023-12-20 20:20:16 -0500

    Summary: Add reusable Pagination component
    - Implement Pagination component with headless UI
    - Component accepts four required parameters:
       1. start: start of page
       2. end: end of page
       3. total: total results
       4. update: a function that updates the above values.
    - The pagination is displayed like so:
       showing i to n of m results. where
       i - start
       n - end
       m - total
    - Ensure accessibility and state management
    - Include TypeScript interfaces for type safety

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,20 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
     ],
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/typedef': [
-      'warn',
-      {
-        arrowParameter: true,
-        variableDeclaration: true,
-        variableDeclarationIgnoreFunction: false
-      }
-    ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        'semi': ['error', 'always'],  // Enforces the use of semicolons
+    },
 };
